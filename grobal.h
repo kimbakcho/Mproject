ad27887f54702ba25ee7fce2e88e1d9c81b42bad
kimbakcho <vngkgk624@naver.com>
2016-03-15 17:30:33 +0900

    test1
    test1

--- grobal.h
+++ grobal.h
@@ -3,10 +3,5 @@
 
 
 
-typedef BOOL	( *FP_CONNECT					) ( HWND, const char *, int, int, int, int );
-typedef BOOL	( *FP_LOGIN					) ( HWND, const char *, const char *, const char *, int, BOOL );
-FP_CONNECT					m_fpConnect;
-FP_LOGIN					m_fpLogin;
-
 #endif // GROBAL
 
